72afb427c69ee5aa9e0943f930341196213251df
Romario <romario.sobreira@hotmail.com>
2018-05-02 20:06:19 +0200

    petit ajout des liens sur le partials menu pour les pages admin & subscribe

--- src/View/partials/menu.php
+++ src/View/partials/menu.php
@@ -37,10 +37,8 @@
                         <a class="nav-link disabled" href="#">Disabled</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                <a href="/admin/">Login</a>
+                 - <a href="/subscribe/">Sign in</a>
             </div>
         </nav>
     </div>
