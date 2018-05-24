e76c961b0ab64f13242dec2fd623d22849dc8155
Romario <romario.sobreira@hotmail.com>
2018-05-24 22:33:04 +0200

    Refactorisation des scripts

--- src/public/js/ajax.js
+++ src/public/js/ajax.js
@@ -1,264 +1,269 @@
-function subscribe_user(username, email, password, confirm_password)
-{
-    $.ajax
-    ({
-        url:'/subscribe_user/',
+/*
+    -- -- -- -- user fonctions -- -- -- --
+    -- -- -- -- user fonctions -- -- -- --
+    -- -- -- -- user fonctions -- -- -- --
+ */
+function subscribe_user(username, email, password, confirm_password) {
+    $.ajax({
+        url: '/subscribe_user/',
         dataType: 'html',
         type: 'POST',
-        data:{username:username, email:email, password:password, confirm_password:confirm_password},
-        success: function(data)
-        {
+        data: {
+            username: username,
+            email: email,
+            password: password,
+            confirm_password: confirm_password
+        },
+        success: function (data) {
             $('.modal-header h5').text("Succès!");
             $('.modal-body').text("Merci, vérifiez vos mails afin de valider votre compte!");
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
         },
-        error: function(xhr, textStatus)
-        {
+        error: function (xhr, textStatus) {
             $('.modal-header h5').text("Erreur!");
             $('.modal-body').text(xhr.responseText);
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
         }
     });
 }
 
-function login_user(username, password)
-{
-    $.ajax
-    ({
-        url:'/login_user/',
-        dataType: 'html',
-        type: 'POST',
-        data:{username:username, password:password},
-        success: function(data)
-        {
-            location.reload();
-        },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
-            $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
-        }
-    });
-}
-
-function logout()
-{
-    $.ajax
-    ({
-        url:'/logout/',
-        dataType: 'html',
-        type: 'POST',
-        success: function(data)
-        {
-            location.reload();
-        },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
-            $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);        }
-    });
-}
-
-$("#file-send").submit(function(e){
-    //e.preventDefault();
-    var formData = new FormData($(this)[0]);
-
+function login_user(username, password) {
     $.ajax({
-        url: '/send-file',
+        url: '/login_user/',
+        dataType: 'html',
         type: 'POST',
-        data: formData,
+        data: {
+            username: username,
+            password: password
+        },
         success: function (data) {
-            $('.modal-header h5').text("Succès!");
-            $('.modal-body').text(data);
+            location.reload();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function logout() {
+    $.ajax({
+        url: '/logout/',
+        dataType: 'html',
+        type: 'POST',
+        success: function (data) {
+            location.reload();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+/*
+    -- -- -- -- posts fonctions -- -- -- --
+    -- -- -- -- posts fonctions -- -- -- --
+    -- -- -- -- posts fonctions -- -- -- --
+ */
+
+function more_posts(count, limit) {
+    $.ajax({
+        url: '/more_posts',
+        type: 'POST',
+        dataType: 'html',
+        data: {
+            count: count,
+            limit: limit
+        },
+        success: function (data) {
+            $('.homepage-blog').fadeOut(300, function () {
+                $('.homepage-blog').html(data);
+            }).fadeIn();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function auteur_posts(auteur) {
+    $.ajax({
+        url: '/auteur_posts',
+        type: 'POST',
+        dataType: 'html',
+        data: {
+            auteur: auteur
+        },
+        success: function (data) {
+            $('.all-posts.the-blog').fadeOut(300, function () {
+                $('.all-posts.the-blog').html(data);
+            }).fadeIn();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function get_tags_articles(tags) {
+    $.ajax({
+        url: '/tags_posts',
+        type: 'POST',
+        dataType: 'html',
+        data: {
+            tags: tags
+        },
+        success: function (data) {
+            $('.all-posts.the-blog').fadeOut(300, function () {
+                $('.all-posts.the-blog').html(data);
+            }).fadeIn();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function get_categorie_posts(categorie) {
+    $.ajax({
+        url: '/categorie_posts',
+        type: 'POST',
+        dataType: 'html',
+        data: {
+            categorie: categorie
+        },
+        success: function (data) {
+            $('.all-posts.the-blog').fadeOut(300, function () {
+                $('.all-posts.the-blog').html(data);
+            }).fadeIn();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function add_post(add_post_datas) {
+    $.ajax({
+        url: '/add_post',
+        type: 'POST',
+        data: add_post_datas,
+        success: function (data) {
+            $('#add-article-modal').modal('hide');
+            refresh_personal_posts();
         },
         cache: false,
         contentType: false,
         processData: false,
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
+        error: function (xhr, textStatus) {
+            $('#modal-triggerer .modal-header h5').text("Erreur!");
+            $('#modal-triggerer .modal-body').text(xhr.responseText);
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
-        }
-    });
-
-    return false;
-});
-
-$('.more-posts').click(function(e){
-   e.preventDefault();
-    var count = 0;
-   $('.homepage-blog .blog-post.box').each(function(){
-       count++;
-   });
-   var newLimit = count + 4;
-   more_posts(count,newLimit);
-
-});
-
-
-// PARTIALS LOAD
-
-function more_posts(count, limit)
-{
-    $.ajax
-    ({
-        url:'/more_posts',
-        type: 'POST',
-        dataType: 'html',
-        data:{count:count,limit:limit},
-        success: function(data)
-        {
-            $('.homepage-blog').fadeOut(300,function(){
-               $('.homepage-blog').html(data);
-            }).fadeIn();
-        },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
-            $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);        }
-    });
-}
-
-// /auteur_posts
-
-$('.get-auteur-posts').click(function(e){
-   e.preventDefault();
-   var auteur = $('.auteurs-area form #auteur').val();
-
-   auteur_posts(auteur);
-});
-
-function auteur_posts(auteur)
-{
-    $.ajax
-    ({
-        url:'/auteur_posts',
-        type: 'POST',
-        dataType: 'html',
-        data:{auteur:auteur},
-        success: function(data)
-        {
-            $('.all-posts.the-blog').fadeOut(300,function(){
-                $('.all-posts.the-blog').html(data);
-            }).fadeIn();
-        },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
-            $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);        }
-    });
-}
-
-$('.tag_search').click(function(e){
-   e.preventDefault();
-   var tags = [];
-
-   $('.tag-item.active').each(function(){
-       tags.push($(this).text());
-   });
-
-   if(tags.length === 0)
-   {
-        get_all_posts();
-   }
-   else
-   {
-        get_tags_articles(tags);
-   }
-});
-
-$('.post-categorie').click(function(e){
-   e.preventDefault();
-
-   var categorie = $(this).text();
-
-   get_categorie_posts(categorie);
-});
-
-$('.tag-item,.post-categorie').click(function(e){
-   e.preventDefault();
-});
-
-function get_tags_articles(tags)
-{
-    $.ajax
-    ({
-        url:'/tags_posts',
-        type: 'POST',
-        dataType: 'html',
-        data:{tags:tags},
-        success: function(data)
-        {
-            $('.all-posts.the-blog').fadeOut(300,function(){
-                $('.all-posts.the-blog').html(data);
-            }).fadeIn();
-        },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
-            $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
         }
     });
 }
 
-function get_categorie_posts(categorie)
-{
-    $.ajax
-    ({
-        url:'/categorie_posts',
+function delete_post(id_article) {
+    $.ajax({
+        url: '/delete_post',
         type: 'POST',
-        dataType: 'html',
-        data:{categorie:categorie},
-        success: function(data)
-        {
-            $('.all-posts.the-blog').fadeOut(300,function(){
-                $('.all-posts.the-blog').html(data);
-            }).fadeIn();
+        data: {
+            id_article: id_article
         },
-        error: function(xhr, textStatus)
-        {
-            $('.modal-header h5').text("Erreur!");
-            $('.modal-body').text(xhr.responseText);
+        success: function (data) {
+            refresh_personal_posts();
+        },
+        error: function (xhr, textStatus) {
+            $('#modal-triggerer .modal-header h5').text("Erreur!");
+            $('#modal-triggerer .modal-body').text(xhr.responseText);
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
         }
     });
 }
 
-function get_all_posts()
-{
-    $.ajax
-    ({
-        url:'/refresh_posts',
+/*
+    -- -- -- -- refresh fonctions -- -- -- --
+    -- -- -- -- refresh fonctions -- -- -- --
+    -- -- -- -- refresh fonctions -- -- -- --
+ */
+
+function refresh_posts() {
+    $.ajax({
+        url: '/refresh_posts',
         type: 'POST',
-        success: function(data)
-        {
-            $('.all-posts.the-blog').fadeOut(300,function(){
+        success: function (data) {
+            $('.all-posts.the-blog').fadeOut(300, function () {
                 $('.all-posts.the-blog').html(data);
             }).fadeIn();
         },
-        error: function(xhr, textStatus)
-        {
+        error: function (xhr, textStatus) {
             $('.modal-header h5').text("Erreur!");
             $('.modal-body').text(xhr.responseText);
             $('#modal-triggerer').modal('show');
-            setTimeout(function(){ $('#modal-triggerer').modal('hide'); },3000);
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        }
+    });
+}
+
+function refresh_personal_posts() {
+    $.ajax({
+        url: '/refresh_personal_posts',
+        type: 'POST',
+        success: function (data) {
+            $('.user-articles-zone').fadeOut(300, function () {
+                $('.user-articles-zone').html(data);
+            }).fadeIn();
+        },
+        error: function (xhr, textStatus) {
+            $('.modal-header h5').text("Erreur!");
+            $('.modal-body').text(xhr.responseText);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
         }
     });
 }
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
-    var called = false;
-    $('#inscription-submit').click(function(e){
+$(document).ready(function () {
+
+    var errors = 0;
+
+    $('#inscription-submit').click(function (e) {
         e.preventDefault();
-        
+
         var $form_username = $('#inscription-form #username').val();
         var $form_email = $('#inscription-form #email').val();
         var $form_password = $('#inscription-form #password').val();
@@ -10,34 +12,166 @@ $(document).ready(function(){
         subscribe_user($form_username, $form_email, $form_password, $form_confirmPassword);
     });
 
-    $('#connect-submit').click(function(e){
+    $('#connect-submit').click(function (e) {
         e.preventDefault();
-        
+
         var $form_username = $('#connection-form #username').val();
         var $form_password = $('#connection-form #password').val();
 
         login_user($form_username, $form_password);
     });
 
-    $('.logout').click(function(e){
+    $('.logout').click(function (e) {
         e.preventDefault();
         logout();
     });
 
-    $('#update-profil-picture').on('change', function(){
+    $('#update-profil-picture').on('change', function () {
         readURL(this);
     });
 
     resizeHeader();
     resizeBlogBoxes();
 
-    $(window).resize(function(){
+    $(window).resize(function () {
         resizeBlogBoxes();
         resizeHeader();
     });
 
-    $('.tag-item').click(function(){
+    $('.tag-item').click(function () {
         switchActiveClass($(this));
+    });
+
+    tinymce.init({
+        selector: 'textarea#add-post-contenu-article',
+        plugins: "autolink",
+        plugins: "link",
+    });
+
+    $(document).on('focusin', function (e) {
+        if ($(e.target).closest(".mce-window").length) {
+            e.stopImmediatePropagation();
+        }
+    });
+
+    $('.add-post-submit').click(function (e) {
+
+        e.preventDefault();
+
+        var titre_article = $('#add-post-titre-article').val().trim();
+        var auteur_article = $('#add-post-auteur-article').val().trim();
+        var extrait_article = $('#add-post-extrait-article').val().trim();
+        var contenu_article = tinyMCE.activeEditor.getContent();
+        var couverture_article = $('.add-post-formulaire input[type=file]')[0].files[0];
+
+        var error_message = "Erreur: ";
+
+        if (titre_article === "") {
+            error_message += "titre, ";
+            errors++;
+        }
+
+        if (auteur_article === "") {
+            error_message += "auteur, ";
+            errors++;
+        }
+
+        if (extrait_article === "") {
+            error_message += "extrait, ";
+            errors++;
+        }
+
+        if (contenu_article === "") {
+            error_message += "contenu, ";
+            errors++;
+        }
+
+        if (couverture_article === undefined) {
+            error_message += "image de couverture ";
+            errors++;
+        }
+
+        if (errors > 0) {
+            error_message += " vide(s)!";
+            $('#modal-triggerer .modal-header h5').text("Erreur!");
+            $('#modal-triggerer .modal-body').text(error_message);
+            $('#modal-triggerer').modal('show');
+            setTimeout(function () {
+                $('#modal-triggerer').modal('hide');
+            }, 3000);
+        } else {
+            var add_post_datas = new FormData();
+
+            add_post_datas.append('titre_article', titre_article);
+            add_post_datas.append('auteur_article', auteur_article);
+            add_post_datas.append('extrait_article', extrait_article);
+            add_post_datas.append('contenu_article', contenu_article);
+            add_post_datas.append('file', couverture_article);
+
+            add_post(add_post_datas);
+        }
+
+        // reset des erreurs si tout est bon
+        errors = 0;
+    });
+
+    $(document).on('click', '.delete-post', function (e) {
+        e.preventDefault();
+        var article_id = parseInt($(this).attr('id'));
+
+        delete_post(article_id);
+    });
+
+    $('.post-categorie').click(function (e) {
+        e.preventDefault();
+
+        var categorie = $(this).text();
+
+        get_categorie_posts(categorie);
+    });
+
+    $('.tag-item,.post-categorie').click(function (e) {
+        e.preventDefault();
+    });
+
+    $('.get-auteur-posts').click(function (e) {
+        e.preventDefault();
+        var auteur = $('.auteurs-area form #auteur').val();
+
+        auteur_posts(auteur);
+    });
+
+    $('.more-posts').click(function (e) {
+        e.preventDefault();
+        var count = 0;
+        $('.homepage-blog .blog-post.box').each(function () {
+            count++;
+        });
+        var newLimit = count + 4;
+        more_posts(count, newLimit);
+
+    });
+
+    $('.tag_search').click(function (e) {
+        e.preventDefault();
+        var tags = [];
+
+        $('.tag-item.active').each(function () {
+            tags.push($(this).text());
+        });
+
+        if (tags.length === 0) {
+            refresh_posts();
+        } else {
+            get_tags_articles(tags);
+        }
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.logo-ressources', {
+        duration: 1000,
+        delay: 200,
+        origin: 'bottom'
     });
 });
 
@@ -45,7 +179,7 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             $('#test').attr('src', e.target.result);
         };
 
@@ -53,11 +187,7 @@ function readURL(input) {
     }
 }
 
-window.sr = ScrollReveal();
-sr.reveal('.logo-ressources', { duration: 1000,delay:200, origin:'bottom' });
-
-function resizeBlogBoxes()
-{
+function resizeBlogBoxes() {
     var firstBlogPostWidth = $('.blog-post:first-of-type').width();
     var heightSecondAndThirdBlogPost = (firstBlogPostWidth / 2) - 5;
     var otherBoxesWidth = $('.blog-post:nth-of-type(4)').width();
@@ -67,21 +197,16 @@ function resizeBlogBoxes()
     $('.blog-post:nth-of-type(1n+4)').height(otherBoxesWidth);
 }
 
-function resizeHeader()
-{
+function resizeHeader() {
     var menuHeight = $('#menu').height();
     $('#header').css('margin-top', menuHeight);
 }
 
-function switchActiveClass(element)
-{
+function switchActiveClass(element) {
     var element = $(element);
-    if(element.hasClass('active'))
-    {
+    if (element.hasClass('active')) {
         element.removeClass('active');
-    }
-    else
-    {
+    } else {
         element.addClass('active');
     }
 }
