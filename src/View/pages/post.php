2f72d3913decc841fbae9a5fc20966289197d91a
Romario <romario.sobreira@hotmail.com>
2018-05-25 20:30:20 +0200

    début de création du template des article de post

--- src/View/pages/post.php
+++ src/View/pages/post.php
@@ -7,11 +7,38 @@
  */
 $post_details = $details->post($title);
 $post = $post_details->fetch();
-$title = $post['titre_article'];
-
 ob_start();
 ?>
-    <p>Voici mon post <b><?= $post['titre_article'] ?></b></p>
+    <div class="post-section section">
+        <div class="post-container container">
+            <div class="post-couverture-area row">
+                <div class="col-12 post-couverture-zone">
+                    <img src="<?php
+                    if ($post['couverture_article'] === "") {
+                        echo "https://via.placeholder.com/800x600.png?text=BLOG";
+                    } else {
+                        echo htmlspecialchars($post['couverture_article']);
+                    };
+                    ?>" class="post-couverture">
+                </div>
+            </div>
+            <div class="post-title-area row">
+                <div class="col-12">
+                    <h2><?= htmlspecialchars($post['titre_article']) ?></h2>
+                </div>
+            </div>
+            <div class="post-extrait-area row">
+                <div class="col-12">
+                    <p><b><?= htmlspecialchars($post['extrait_article']) ?></b></p>
+                </div>
+            </div>
+            <div class="post-contenu-area row">
+                <div class="col-12">
+                    <?= $post['contenu_article'] ?>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 $post_details->closeCursor();
 $content = ob_get_clean();
