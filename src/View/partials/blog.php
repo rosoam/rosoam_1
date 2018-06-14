81146cf14cecf7896f8226b5c8bd5348d4451049
Romario <romario.sobreira@hotmail.com>
2018-06-14 16:30:57 +0200

    changements css

--- src/View/partials/blog.php
+++ src/View/partials/blog.php
@@ -12,26 +12,28 @@ foreach ($fetch_blog as $post)
 {
     $this->newPost($post);
     ?>
-<div class="col blog blog-blog-post ">
-    <a href="/posts/<?= htmlspecialchars($this->_article->getSlug()) ?>" class="blog-post-link">
-        <img src="<?= $this->_article->getCouverture() === "" ? "https://via.placeholder.com/800x600.png?text=BLOG" : htmlspecialchars($this->_article->getCouverture()); ?>" class="blog-post-couverture" alt="couverture de l'article">
-    </a>
+<div class="blog-box">
+    <div class="blog blog-blog-post ">
+        <a href="/posts/<?= htmlspecialchars($this->_article->getSlug()) ?>" class="blog-post-link">
+            <img src="<?= $this->_article->getCouverture() === "" ? "https://via.placeholder.com/800x600.png?text=BLOG" : htmlspecialchars($this->_article->getCouverture()); ?>" class="blog-post-couverture" alt="couverture de l'article">
+        </a>
+    </div>
     <div class="blog-blog-post-body">
         <a href="/posts/<?= htmlspecialchars($this->_article->getSlug()) ?>" class="blog-post-link"></a>
         <span class="blog-blog-infos"><span class="categorie">
             <?php
-                $categories = $this->_post->categories($post['id_article']);
-                $this->newCategorie($categories->fetch());
-                $count = $categories->rowCount();
-                if($count > 0)
-                {
-                    echo htmlspecialchars($this->_categorie->getNom());
-                }
-                else
-                {
-                    echo "Aucune categorie";
-                }
-                $categories->closeCursor();
+            $categories = $this->_post->categories($post['id_article']);
+            $this->newCategorie($categories->fetch());
+            $count = $categories->rowCount();
+            if($count > 0)
+            {
+                echo htmlspecialchars($this->_categorie->getNom());
+            }
+            else
+            {
+                echo "Aucune categorie";
+            }
+            $categories->closeCursor();
             ?></span> | <?= htmlspecialchars($this->_article->getPublication())  ?> BY <?= htmlspecialchars($this->_article->getAuteur()) ?></span><br>
         <h3><?= htmlspecialchars($this->_article->getTitre()) ?></h3>
     </div>
